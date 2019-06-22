da2c64cf896244770c1aee73b7c3efb06d2beefa
demetrius <dsterlyagov@gmail.com>
2019-06-22 13:36:05 +0300

    create first route

---
+++ public_html/application/config/routes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Дима
+ * Date: 21.06.2019
+ * Time: 12:52
+ */
+return[
+    '' =>[
+        'controller' => 'main',
+        'action' => 'index',
+
+    ],
+    'account/login' =>[
+        'controller' => 'account',
+        'action' => 'login',
+    ],
+    'news/show' => [
+        'controller' => 'new',
+        'action' => 'show'
+    ],
+];
--- public_html/application/core/Router.php
+++ public_html/application/core/Router.php
@@ -10,17 +10,44 @@
     {
         protected $routes = [];
         protected $params = [];
+
         function __construct()
-    {
-    }
-    public function add(){
+        {
+            $arr = require 'application/config/routes.php';
+            foreach ($arr as $key => $val) {
+                $this->add($key, $val);
+            }
+            //debug($this->routes);
+        }
 
-    }
-    public function match(){
+        public function add($route, $params)
+        {
+            $route = '#^' . $route . '$#';
+            $this->routes[$route] = $params;
 
-    }
-    public function run(){
-            echo 'start';
-    }
+        }
 
+        public function match()
+        {
+            $url = trim($_SERVER['REQUEST_URI'], '/');
+            foreach ($this->routes as $route => $params) {
+                if (preg_match($route, $url, $matches)) {
+                    $this->params = $params;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public function run()
+        {
+            if ($this->match()) {
+                $controller = 'application\controllers\\' . ucfirst($this->params['controller']) . 'Controller.php';
+                if (class_exists($controller)) {
+                    echo 'Ok';
+                } else {
+                    echo 'Маршрут не найден';
+                }
+            }
+        }
     }
